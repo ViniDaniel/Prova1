538865503cdbf99574f1257a53fa988d8da36e30
Vinícius Daniel <emailfake@123.com>
2024-10-01 19:46:35 -0300

    Questão 4 - Continuação da questão 4, esqueci q era o nome da app, atualizando para App de nome todos

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
-    return HttpResponse('Olá mundo')
+    return HttpResponse('App de nome todos')
 # Create your views here.
